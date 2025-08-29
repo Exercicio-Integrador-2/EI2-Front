ebaae26bb9dc707074ae568dfb19cf85c651b907
Maria Eduarda Gotuzzo da Silva <m.gotuzzo@edu.pucrs.br>
2025-08-29 11:36:46 -0300

    select funcionario pronto

--- Front/src/app/shared/header/header.component.ts
+++ Front/src/app/shared/header/header.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, OnInit } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { EmployeeDTO } from '../../models/get/employee.dto';
+import { EmployeeService } from '../../services/employee.service';
+import { SelectedEmployeeService } from '../../services/selectedEmployeeService';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [MatFormFieldModule, MatSelectModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  logoPath: string = "assets/img/logo.png"
+export class HeaderComponent implements OnInit {
+  logoPath: string = "assets/img/logo.png";
 
-  selected = '1234657';
+  employees: EmployeeDTO[] = [];
+  selected: EmployeeDTO | null = null;
+
+  constructor(
+    private employeeService: EmployeeService,
+    private selectedEmployeeService: SelectedEmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    this.employeeService.getAll().then(obs => {
+      obs.subscribe(data => {
+        this.employees = data;
+      });
+    });
+  }
+
+  onUserChange(employee: EmployeeDTO) {
+    this.selected = employee;
+    this.selectedEmployeeService.setEmployee(employee);
+  }
 }
