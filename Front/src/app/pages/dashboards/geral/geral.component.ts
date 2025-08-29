6f34c27647807f2a5f996d97a0355479cf77f54a
Eduarda Gotuzzo <m.gotuzzo@edu.pucrs.br>
2025-08-29 11:36:35 -0300

    adicionando tabelas especificas

--- Front/src/app/pages/dashboards/geral/geral.component.ts
+++ Front/src/app/pages/dashboards/geral/geral.component.ts
@@ -1,37 +1,152 @@
 import {Component} from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+export const NOTEBOOKS_DATA = [
+  { 
+    nome: 'Dell Latitude 5420', 
+    patrimonio: 'NTB00123', 
+    desc: 'Core i5, 16GB RAM, 256GB SSD', 
+    data: '15/05/2024' 
+  },
+  { 
+    nome: 'HP Probook 440 G8', 
+    patrimonio: 'NTB00124', 
+    desc: 'Ryzen 5, 8GB RAM, 512GB SSD', 
+    data: '22/08/2024' 
+  },
+  { 
+    nome: 'Lenovo ThinkPad T14', 
+    patrimonio: 'NTB00125', 
+    desc: 'Core i7, 16GB RAM, 1TB SSD', 
+    data: '10/01/2025' 
+  },
+  { 
+    nome: 'MacBook Air M2', 
+    patrimonio: 'NTB00126', 
+    desc: 'Chip M2, 8GB RAM, 256GB SSD', 
+    data: '05/03/2025' 
+  },
 ];
+
+export const SALAS_DATA = [
+  { 
+    nome: 'Sala de Reuniões Atlântida', 
+    num: 101, 
+    capacidade: 8, 
+    projetor: 'Sim' 
+  },
+  { 
+    nome: 'Sala de Foco 1', 
+    num: 102, 
+    capacidade: 4, 
+    projetor: 'Não' 
+  },
+  { 
+    nome: 'Sala de Treinamento Alfa', 
+    num: 205, 
+    capacidade: 20, 
+    projetor: 'Sim' 
+  },
+  { 
+    nome: 'Auditório Beta', 
+    num: 310, 
+    capacidade: 50, 
+    projetor: 'Sim' 
+  },
+];
+
+export const LABS_DATA = [
+  { 
+    nome: 'Laboratório de Redes', 
+    qtd_pc: 20, 
+    desc: 'Equipado com switches e roteadores Cisco' 
+  },
+  { 
+    nome: 'Laboratório de Hardware', 
+    qtd_pc: 15, 
+    desc: 'Bancadas com fontes, osciloscópios e multímetros' 
+  },
+  { 
+    nome: 'Laboratório de Programação', 
+    qtd_pc: 25, 
+    desc: 'Computadores com múltiplos IDEs e compiladores' 
+  },
+  { 
+    nome: 'Laboratório de Design Gráfico', 
+    qtd_pc: 18, 
+    desc: 'iMacs com suíte Adobe Creative Cloud instalada' 
+  },
+];
+
 @Component({
   selector: 'app-geral',
-  imports: [ MatTableModule, MatIconModule],
+  imports: [ MatTableModule, MatIconModule, MatButtonToggleModule, CommonModule],
   templateUrl: './geral.html',
   styleUrl: './geral.scss'
 })
-export class GeralComponent {
-  displayedColumns: string[] = ['num', 'nome', 'desc', 'tipo', 'edit'];
-  dataSource = ELEMENT_DATA;
+export class GeralComponent{  
+  selectedView: string = 'notes'; 
 
-  editItem(element: any) {
-    console.log('Editando o item:', element);
+  notebooksDataSource = new MatTableDataSource<any>();
+  notebooksDisplayedColumns: string[] = ['nome', 'patrimonio', 'desc', 'data', 'edit'];
+
+  salasDataSource = new MatTableDataSource<any>();;
+  salasDisplayedColumns: string[] = ['nome', 'num', 'capacidade', 'projetor', 'edit'];
+
+  labsDataSource = new MatTableDataSource<any>();
+  labsDisplayedColumns: string[] = ['nome', 'qtd_pc', 'desc', 'edit'];
+
+
+  constructor() {}
+
+  ngOnInit() {
+    this.fetchNotebooks();
+  }
+
+  onViewChange(view: string) {
+    this.selectedView = view;
+    
+    // Carrega os dados correspondentes à nova visão
+    switch (view) {
+      case 'notes':
+        this.fetchNotebooks();
+        break;
+      case 'salas':
+        this.fetchSalas();
+        break;
+      case 'labs':
+        this.fetchLabs();
+        break;
+    }
+  }
+
+  fetchNotebooks() {
+    console.log("Buscando dados de Notebooks...");
+
+    // dados mockados:
+    this.notebooksDataSource.data = NOTEBOOKS_DATA;
+  }
+
+  fetchSalas() {
+    console.log("Buscando dados de Salas...");
+    
+    // dados mockados:
+    this.salasDataSource.data = SALAS_DATA;
+  }
+
+  fetchLabs() {
+    console.log("Buscando dados de Laboratórios...");
+    
+    // dados mockados:
+    this.labsDataSource.data = LABS_DATA;
+  }
+
+  editItem(element: any, type: string) {
+    console.log(`Editando item do tipo ${type}:`, element);
   }
 }
